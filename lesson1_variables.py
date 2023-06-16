7332a977a8a3ca06ae3e27d89a5c4758ad82a53c
dang <110548556+hongdangn@users.noreply.github.com>
2023-06-17 01:12:03 +0700

    Update lesson1_variables.py

--- lesson1_variables.py
+++ lesson1_variables.py
@@ -7,14 +7,14 @@
  print(type(name))
  print("Hong Dang " + name)
 
-// class integer
+ // class integer
  age = 21
  age = age + 10
  age += 10
  print("Me when I am such a goo husband: " + str(age))
  print(type(age))
 
-// class float
+ // class float
  height = 170.5
  print(type(height))
  print(height)
@@ -22,7 +22,20 @@
  height += 1.411
  print(height)
 
-// class bool
+ // class bool
  human_heart = True
  print(human_heart)
  print(type(human_heart))
+ 
+ // multiple assignment
+ // first type: name = "Dang", age = 19, attractive = True is exactly the same as
+ name, age, attractive = "Dang", 19, True
+ print(name)
+ print(age)
+ print(attractive)
+ 
+ // second type: nguyen = 19, hong = 19, dang = 19 is exactly the same as 
+  nguyen = hong = dang = 19
+ print(nguyen)
+ print(hong)
+ print(dang)
