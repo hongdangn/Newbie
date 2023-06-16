906bb71e9b794ed6460fcb3674112a47746a966e
dang <110548556+hongdangn@users.noreply.github.com>
2023-06-17 01:37:56 +0700

    Create lesson2_stringsmethod.py

---
+++ lesson2_stringsmethod.py
@@ -0,0 +1,12 @@
+name = "dang"
+
+print(len(name)) #length of the name variable
+print(name.find("a")) #find the location of character "a" in the name variable
+print(name.capitalize()) #capitalize first letter of the name
+print(name.upper()) #all letters in the name are uppercase
+print(name.lower()) #all letters in the name are lowercase
+print(name.isdigit()) #if all the leters are digits, return true, else, return false
+print(name.isalpha()) #if all the letters are the alphabetical characters, return true, else return false
+print(name.count("a")) #count how many "a" characters in the name
+print(name.replace("a", "Q")) #first letter in the parentheness will be replaced by the second one
+print(name*4) #print the name 4 times
