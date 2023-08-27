1e60d9ba84c14e40c6a6007b06331da8f595e815
dang <110548556+hongdangn@users.noreply.github.com>
2023-08-27 10:32:29 +0700

    Create lesson1_variables.py

---
+++ note_about_python/lesson1_variables.py
@@ -0,0 +1,41 @@
+ // class string
+ first_name = "Nguyen"
+ last_name = "Dang"
+ full_name = first_name + last_name;
+ print("Bye " + full_name)
+ print(first_name + " " + last_name)
+ print(type(name))
+ print("Hong Dang " + name)
+
+ // class integer
+ age = 21
+ age = age + 10
+ age += 10
+ print("Me when I am such a goo husband: " + str(age))
+ print(type(age))
+
+ // class float
+ height = 170.5
+ print(type(height))
+ print(height)
+ print("My height is: " + str(height))
+ height += 1.411
+ print(height)
+
+ // class bool
+ human_heart = True
+ print(human_heart)
+ print(type(human_heart))
+ 
+ // multiple assignment
+ // first type: name = "Dang", age = 19, attractive = True is exactly the same as
+ name, age, attractive = "Dang", 19, True
+ print(name)
+ print(age)
+ print(attractive)
+ 
+ // second type: nguyen = 19, hong = 19, dang = 19 is exactly the same as 
+  nguyen = hong = dang = 19
+ print(nguyen)
+ print(hong)
+ print(dang)
