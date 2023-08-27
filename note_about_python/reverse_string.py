8aeb1a632d4c909b601a80bef28a1b347c55de6e
Hong Dang <110548556+hongdangn@users.noreply.github.com>
2023-08-27 15:44:08 +0700

    Create reverse_string.py

---
+++ note_about_python/reverse_string.py
@@ -0,0 +1,13 @@
+def reverse_1(string):
+    new_string = ""
+    for i in range(len(string)):
+        new_string += string[len(string) - i - 1]
+    return new_string
+    
+def reverse_2(string):
+    return reverse(string)
+    
+def reverse_3(string):
+    return string[8::-1]
+    
+string = "hong dang"
